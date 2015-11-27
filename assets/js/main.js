adcd10707de6835f873edc8d2bb6211d23c3cb02
elvis-poggi <poggi.elvis@gmail.com>
2015-11-27 18:30:54 +0100

    removed some animations, links and fonts to speed up the page successfully

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,51 +13,51 @@ $("ul.qcontrols li").click(function(){
 });
 
 
-//WOW Scroll Spy
-var wow = new WOW({
-    //disabled for mobile
-    mobile: false
-});
-wow.init();
+////WOW Scroll Spy
+//var wow = new WOW({
+//    //disabled for mobile
+//    mobile: false
+//});
+//wow.init();
+//
+//jQuery(document).ready(function( $ ) {
+//        $('.circle').circleProgress({
+//        value: 0.70,
+//        startAngle: 4.75,
+//        size: 200,
+//        fill: { gradient: ["#FFFFFF"] }
+//      });
+//        $('.two').circleProgress({
+//          value: 0.75,
+//        });
+//        $('.three').circleProgress({
+//          value: 0.50,
+//        });
+//        $('.four').circleProgress({
+//          value: 0.90,
+//        });
 
-jQuery(document).ready(function( $ ) {
-        $('.circle').circleProgress({
-        value: 0.70,
-        startAngle: 4.75,
-        size: 200,
-        fill: { gradient: ["#FFFFFF"] }
-      });
-        $('.two').circleProgress({
-          value: 0.75,
-        });
-        $('.three').circleProgress({
-          value: 0.50,
-        });
-        $('.four').circleProgress({
-          value: 0.90,
-        });
-
-   //Clients carousel Slider
-    $('#clients-carousel').owlCarousel({
-        navigation: false, // Show next and prev buttons
-        slideSpeed: 400,
-        paginationSpeed: 800,
-        autoPlay: true,
-        pagination : false,
-        items : 4,
-        itemsCustom : false,
-        itemsDesktop : [1199,4],
-        itemsDesktopSmall : [980,3],
-        itemsTablet: [768,2],
-        itemsTabletSmall: false,
-        itemsMobile : [479,1],
-    });
-       $('#menu').click(function(){
-       $('#menu').not(this).removeClass("active");
-       $(this).toggleClass("active");
-      });
-
-  });
+//   //Clients carousel Slider
+//    $('#clients-carousel').owlCarousel({
+//        navigation: false, // Show next and prev buttons
+//        slideSpeed: 400,
+//        paginationSpeed: 800,
+//        autoPlay: true,
+//        pagination : false,
+//        items : 4,
+//        itemsCustom : false,
+//        itemsDesktop : [1199,4],
+//        itemsDesktopSmall : [980,3],
+//        itemsTablet: [768,2],
+//        itemsTabletSmall: false,
+//        itemsMobile : [479,1],
+//    });
+//       $('#menu').click(function(){
+//       $('#menu').not(this).removeClass("active");
+//       $(this).toggleClass("active");
+//      });
+//
+//  });
 
   
 //ScrollTop
